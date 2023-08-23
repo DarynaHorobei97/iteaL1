b6fbedce5a38ca10627f9fdf1584fe82b6cd67e4
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-08-23 21:14:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+
+    static void showMessage(String message){
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+       showMessage("Hi");
+    }
+}
