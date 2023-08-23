4dbc52932e943d3573ec75cce5af84eb31fe9bac
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-08-23 21:24:28 +0300

    Added one more object1

--- src/p1/Car1.java
+++ src/p1/Car1.java
@@ -17,8 +17,5 @@ public class Car1 {
     public static void main(String[] args) {
         Car1 car = new Car1();
         Car1 car1 = new Car1();
-        Car1 car2 = new Car1();
-        car.sumOfTwoNumbers(1, 5);
-
     }
 }
